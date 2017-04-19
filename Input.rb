1075f91af989abe85a87f7c3f3c19ee21b620c2e
legrand <oceane.legrand@etudiant.univ-lille1.fr>
2017-04-19 11:33:55 +0200

    entree

--- Input.rb
+++ Input.rb
@@ -1,9 +1,13 @@
 require_relative('Direction.rb')
 
-puts("where do you wan to go ?")
-direction = Direction.key(gets.chomp)
-if direction != nil
-  puts "you want to go to "+direction.to_s.downcase
-else
-  puts "not a good direction"
+
+def get_direction
+  puts("where do you wan to go ?")
+  direction = Direction.key(gets.chomp)
+  if direction != nil
+    puts "you want to go to "+direction.to_s.downcase
+  else
+    puts "not a good direction"
+  end
+  direction
 end
--- a-maze-ing.rb
+++ a-maze-ing.rb
@@ -9,16 +9,12 @@ class Maze
   def initialize(length, height)
     self.cases = Array.new(length) {Array.new(height)}
   end
-  def enter(direction)
-    if (["N", "E", "S", "W"].include?direction)
-      print("You entered through the entrance " + direction + "\n")
-    else
-      print("No such entrie " + direction + "\n")
-    end
+  def enter()
+    direction = get_direction
   end
 end
 
 x_size = 9
 y_size = 9
 maze = Maze.new(x_size, y_size)
-maze.enter(gets)
+maze.enter()
