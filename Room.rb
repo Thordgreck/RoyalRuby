622aa9ed6e9feff457cd554fffa242110af004f0
Aymeric César <aymeric@smartmeup.org>
2017-04-19 10:43:52 +0200

    basic room

--- Room.rb
+++ Room.rb
@@ -1,5 +1,14 @@
+require_relative 'Door.rb'
+require_relative 'Wall.rb'
+
 class Room
   def initialize()
-    print("Whalah c'est init laijen!" + "\n")
+    self.walls = Array.new(4) {Wall.new()}
+    self.doors = Array.new(4) {Door.new()}
+  end
+
+  def describe()
+    self.walls.each { |a| a.describe()}
+    self.doors.each { |a| a.describe()}
   end
 end
