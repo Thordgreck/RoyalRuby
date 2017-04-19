ff9a62ada0d2072bdddd5aadb6bb6242f1bdbf18
Guillaume Bovier-Lapierre <guillaumebovier-lapierre@captiveportal-10-100-5-153.local.su.se>
2017-04-19 11:52:37 +0200

    Enemies name

--- Enemy.rb
+++ Enemy.rb
@@ -8,26 +8,31 @@ class Enemy
     end
     
     class NormalEnemy
+        @name = "Enemy"
         @helpDesc = "Just a normal enemy"
     end
     
-    class SwordMan
+    class Swordman
         # Sword
+        @name = "Swordman"
         @helpDesc = "Soldier with a sword, care his hit remove you 2Hp!"
     end
     
-    class Armored
+    class ArmoredGuy
         # Armor
+        @name = "Armored guy"
         @helpDesc = "An armored guy, he tank one of your hit."
     end
     
     class Knight
         # Armor + Sword
+        @name = "Knight"
         @helpDesc = "This knight have an armor and a sword, he tank one of your hit and deals 2Hp damage."
     end
     
     class Magician
         # Use magic spell, ignore players armor
+        @name = "Magician"
         @helpDesc = "Magic spell ignore armor!"
     end
 end
