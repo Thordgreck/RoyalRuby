27d0606525cb08aa66ca3887ce287ae7e218073e
legrand <oceane.legrand@etudiant.univ-lille1.fr>
2017-04-19 10:42:17 +0200

    correction direction

--- Direction.rb
+++ Direction.rb
@@ -1,6 +1,29 @@
 class Direction
-  NORTH
-  EST
-  SOUTH
-  WEST
+    def Direction.add_item(key,value)
+        @hash ||= {}
+        @hash[key]=value
+    end
+
+    def Direction.const_missing(key)
+        @hash[key]
+    end
+
+    def Direction.each
+        @hash.each {|key,value| yield(key,value)}
+    end
+
+    Direction.add_item :NORTH, "n"
+    Direction.add_item :EST, "e"
+    Direction.add_item :SOUTH, "s"
+    Direction.add_item :WEST, "w"
 end
+
+#That's it! We can now use our enum :
+
+#my_Direction = Direction::NORTH if some_condition
+
+#And we can loop :
+
+#Direction.each do |key,value|
+#  do_something_with_value(value)
+#end
