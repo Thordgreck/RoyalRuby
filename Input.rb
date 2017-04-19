7e52e2aeef04818bf2f5a5275d461a5fdef5496f
Aymeric César <aymeric@smartmeup.org>
2017-04-19 11:39:10 +0200

    gimme some downcase

--- Input.rb
+++ Input.rb
@@ -2,7 +2,7 @@ require_relative('Direction.rb')
 
 def get_direction
   puts("where do you wan to go ?")
-  direction = Direction.key(gets.chomp)
+  direction = Direction.key(gets.chomp.downcase)
   if direction != nil
     puts "you want to go to "+direction.to_s.downcase
   else
