32714ae8af7d7686a29dc04904c510b6f604b356
Aymeric César <aymeric@smartmeup.org>
2017-04-20 19:50:00 +0200

    method needed in direction to comport like an enum

--- Direction.rb
+++ Direction.rb
@@ -1,25 +1,29 @@
 class Direction
-    def Direction.add_item(key,value)
-        @hash ||= {}
-        @hash[key]=value
-    end
+  def Direction.add_item(key,value)
+    @hash ||= {}
+    @hash[key]=value
+  end
 
-    def Direction.each
-        @hash.each {|key,value| yield(key,value)}
-    end
+  def Direction.const_missing(key) #DO NOT CHANGE THIS NAME!!!
+    @hash[key]
+  end
 
-    def Direction.key(value)
-      @hash.key(value)
-    end
+  def Direction.each
+    @hash.each {|key,value| yield(key,value)}
+  end
 
-    def Direction.value(key)
-      @hash[key]
-    end
+  def Direction.key(value)
+    @hash.key(value)
+  end
 
-    Direction.add_item :NORTH, 0
-    Direction.add_item :EST, 1
-    Direction.add_item :SOUTH, 2
-    Direction.add_item :WEST, 3
+  def Direction.value(key)
+    @hash[key]
+  end
+
+  Direction.add_item :NORTH, 0
+  Direction.add_item :EST, 1
+  Direction.add_item :SOUTH, 2
+  Direction.add_item :WEST, 3
 end
 
 #That's it! We can now use our enum :
