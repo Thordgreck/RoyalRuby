deb69ed44f7aca28da823d5527ba83c9484e5b9d
Aymeric César <aymeric@smartmeup.org>
2017-05-12 19:16:33 +0200

    add game description

--- a-maze-ing.rb
+++ a-maze-ing.rb
@@ -5,7 +5,13 @@ require_relative 'Game.rb'
 require_relative 'Exception.rb'
 
 again = TRUE
-print("---> INTRO TEXT\n")
+print("A long time ago in a galaxy far, far away, Sandra, a great adventurer
+that loves to explore pyramids and other old places to find shiny treasures and
+a lot of gold, is called in front of his king. The Royal Ruby that has been
+stolen by a dragon, and Sandra has been chosen to be the one to retrieve it!
+You will impersonate her, but be aware that there are monsters that want to
+kill you and eat you. You will also find objects that will help you fight
+monsters, survive, find the treasure and leave the maze.\n".bold_blue())
 while again
   begin
     Game.reset()
