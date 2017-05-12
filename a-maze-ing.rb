79124c8f80ff353e3f012a9dbd216f1e47988c02
Aymeric César <aymeric@smartmeup.org>
2017-05-12 19:29:17 +0200

    add win/lost count

--- a-maze-ing.rb
+++ a-maze-ing.rb
@@ -12,15 +12,20 @@ stolen by a dragon, and Sandra has been chosen to be the one to retrieve it!
 You will impersonate her, but be aware that there are monsters that want to
 kill you and eat you. You will also find objects that will help you fight
 monsters, survive, find the treasure and leave the maze.\n".bold_blue())
+won = 0
+lost = 0
 while again
   begin
     Game.reset()
     Game.mainloop(IA.new())
   rescue WonGame => e
     puts "You win !!!!!!!!!!!!!".bold_cyan()
+    won += 1
   rescue LooseGame => e
     puts "You left your wife, your son, your home and this world for a better life...".bold_red()
+    lost += 1
   rescue StandardError => e
+    p won lost
     raise e
   end
   again = ask_new_game()
