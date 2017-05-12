6b8e76bd9f0ec45e7cff9979f7d9819870f8f10e
Aymeric César <aymeric@smartmeup.org>
2017-05-12 16:37:49 +0200

    fix full dragon attack and main goes static

--- a-maze-ing.rb
+++ a-maze-ing.rb
@@ -19,31 +19,31 @@ class Game
 
   def Game.mainloop(player)
     while !player.isDead && !player.hasWon
-      if (@current.nil?)
-        puts ("Created room at " + @x.to_s + " " + @y.to_s).white()
-        @current = RoomFactory.newRoom(@x, @y)
-        if @current.wakes_dragon()
-          @dragon = 15
+      if (@@current.nil?)
+        puts ("Created room at " + @@x.to_s + " " + @@y.to_s).white()
+        @@current = RoomFactory.newRoom(@@x, @@y)
+        if @@current.wakes_dragon()
+          @@dragon = 15
         end
       end
-      puts ("Went in room " + @x.to_s + " " + @y.to_s).white()
-      @current.describe()
-      if @dragon == 16
-        @dragon -= 1
+      puts ("Went in room " + @@x.to_s + " " + @@y.to_s).white()
+      @@current.describe()
+      if @@dragon == 16
+        @@dragon -= 1
         puts "You woke the dragon!!!".red()
         player.takeDamage(1, TRUE)
-      elsif @dragon >= 6
-        @dragon -= 1
+      elsif @@dragon >= 6
+        @@dragon -= 1
         puts "The dragon attacks you!!!".red()
         player.takeDamage(1, TRUE)
-        if @dragon == 0
-          @dragon -= 1
+        if @@dragon == 0
+          @@dragon -= 1
         end
-      elsif @dragon >= 2
-        @dragon -= 1
+      elsif @@dragon >= 2
+        @@dragon -= 1
         puts "The dragon went back to check on his treasure, but he might be back!!!".red()
-      elsif @dragon == 1
-        @dragon -= 1
+      elsif @@dragon == 1
+        @@dragon -= 1
         if rand(2) == 0
           puts "The dragon went back to eat you!\nHe brought you back to the treasure room and reduced your Hp to 1!!".red()
           player.dragonAttack()
@@ -63,7 +63,7 @@ class Game
         return
       end
       puts "You look around in the room, maybe you will find something?".yellow()
-      @current.loot(player, fought_monster)
+      @@current.loot(player, fought_monster)
       if(!player.inventory.empty?)
         puts "Do you want to see your inventory (y/N)?".magenta()
         if (result = ask_yes_not())
@@ -74,24 +74,24 @@ class Game
       puts
       can_move = FALSE
       direction = nil
-      avail = @current.availableDoors()
+      avail = @@current.availableDoors()
       while !can_move
         puts "Select a direction in the availables:".magenta()
         direction = player.chooseDirection(avail)
-        if @current.canMove(direction)
-          can_move = @current.tryMove(direction, player)
+        if @@current.canMove(direction)
+          can_move = @@current.tryMove(direction, player)
         end
         avail.delete_at(avail.index(direction))
       end
-      @current = RoomFactory.getRoom(@x, @y, direction)
+      @@current = RoomFactory.getRoom(@@x, @@y, direction)
       if direction == Direction::NORTH
-        @y += 1
+        @@y += 1
       elsif direction == Direction::SOUTH
-        @y -= 1
+        @@y -= 1
       elsif direction == Direction::EAST
-        @x += 1
+        @@x += 1
       elsif direction == Direction::WEST
-        @x -= 1
+        @@x -= 1
       end
     end
   end
